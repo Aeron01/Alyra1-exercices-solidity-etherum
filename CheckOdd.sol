e6483a56037870b328b7e27c5e4550bc7204aadb
Aeron01 <one.soulreaper@gmail.com>
2021-05-12 15:35:14 +0200

    avec les corrections

--- CheckOdd.sol
+++ CheckOdd.sol
@@ -11,3 +11,17 @@ contract CheckOdd {
     } 
   }
 }
+
+
+// correction
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+
+contract CheckOdd {
+  function check(uint256 number) public pure returns (bool) {
+    return number % 2 == 0 ? false : true;
+  }
+}
--- TheOne.sol
+++ TheOne.sol
@@ -13,3 +13,15 @@ function one () public view returns(uint256) {
     return _one;
   }
 }
+
+// correction
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TheOne {
+  function one () public pure returns(uint256) {
+    return 1;
+  }
+}
